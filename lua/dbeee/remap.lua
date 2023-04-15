3e2e13b92e8ab3b3063dbe8edb486721f19f2e93
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-04-15 15:07:33 +0545

    Lsp-zero for lsp server

--- lua/dbeee/remap.lua
+++ lua/dbeee/remap.lua
@@ -15,4 +15,5 @@ vim.opt.expandtab = true
 vim.wo.relativenumber = true
 vim.wo.number = true
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>wq", vim.cmd.wq)
