5f6814d0275ad8f3c5b6f7585b8e5979611d7ef6
Dbeee-Acharya <dbeee.acharya@gmail.com>
2023-04-18 08:38:26 +0545

    new config

--- lua/dbeee/packer.lua
+++ lua/dbeee/packer.lua
@@ -39,9 +39,23 @@ return require('packer').startup(function(use)
 
   -- lsp
   use {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig"
-  }
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'},             -- Required
+      {                                      -- Optional
+      'williamboman/mason.nvim',
+      run = function()
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+  }
+}
 end)
--- lua/dbeee/remap.lua
+++ lua/dbeee/remap.lua
@@ -18,3 +18,5 @@ vim.wo.number = true
 vim.keymap.set("n", "<leader>w", vim.cmd.w)
 vim.keymap.set("n", "<leader>q", vim.cmd.q)
 vim.keymap.set("n", "<leader>wq", vim.cmd.wq)
+
+ 
